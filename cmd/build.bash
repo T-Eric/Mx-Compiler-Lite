19e6fa11d296c930ee5830aae7ab388865213b4f
T-Eric <1666384703@qq.com>
2024-08-12 21:13:43 +0800

    trial 3: support oj sema test

--- cmd/build.bash
+++ cmd/build.bash
@@ -1,6 +1,6 @@
 set ff=UNIX
 mkdir -p bin
-find src -name *.java | javac -d bin -cp /ulib/java/antlr-4.13.1-complete.jar @/dev/stdin
+find src -name *.java | javac -d bin -cp /ulib/antlr-4.13.1-complete.jar @/dev/stdin
 # find src -name *.java | javac -d bin -cp antlr/antlr-4.13.1-complete.jar @/dev/stdin
 # echo "Built successfully."
 # source cmd/sema.bash
--- cmd/sema.bash
+++ cmd/sema.bash
@@ -1,2 +1,2 @@
-cat | java -cp /ulib/java/antlr-4.13.1-complete.jar:bin sema
+cat | java -cp /ulib/antlr-4.13.1-complete.jar:bin sema
 # cat | java -cp antlr/antlr-4.13.1-complete.jar:bin sema
